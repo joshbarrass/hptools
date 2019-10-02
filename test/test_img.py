39c71d68466c2407589fde8da309615f2311112e
WORD559 <josh.barrass.work@gmail.com>
2019-10-02 23:01:41 +0100

    Test IMG->PIL conversion

--- test/test_img.py
+++ test/test_img.py
@@ -1,6 +1,11 @@
 import unittest
+import os
+
+from PIL import Image
 
 import img
+
+test_dir = os.path.dirname(__file__)
 
 class ImgTests(unittest.TestCase):
     def test_bit_selector(self):
@@ -28,3 +33,15 @@ class ImgTests(unittest.TestCase):
                 print(a[i,j])
                 print(i+j+1)
                 self.assertTrue(list(a[i,j]) == [2*i+j+1]*3)
+
+    def test_convert_IMG(self):
+        orig_im = Image.open(os.path.join(test_dir, "test.tif")).convert("RGB")
+        im = img.convert_IMG(img.read_IMG(os.path.join(test_dir, "test.testimg")), orig_im.size)
+
+        self.assertEqual(im.size, orig_im.size)
+
+        im_pix = im.load()
+        orig_pix = orig_im.load()
+        for y in range(im.size[1]):
+            for x in range(im.size[0]):
+                self.assertEqual(im_pix[x,y], orig_pix[x,y])
